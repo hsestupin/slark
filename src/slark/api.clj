c1acd8a66fc319d659e654e318ea0dae47ce7bd7
hsestupin <stupin.sergey@gmail.com>
2016-06-07 01:25:55 +0300

    major update. Add setWebhook implementation

--- src/slark/api.clj
+++ src/slark/api.clj
@@ -8,19 +8,66 @@
 (defn get-token []
   (env :telegram-bot-token))
 
+(defn- build-telegram-api-url
+  [{:keys [token] :or {token (get-token)} :as params} url-suffix]
+  (str base-url token url-suffix))
+
+(defn- extract-telegram-payload
+  [response]
+  (-> response
+      :body
+      (json/read-str :key-fn keyword)))
+
+(defn- get-result
+  [payload-only response]
+  (if payload-only
+      (extract-telegram-payload response)
+      response))
+
+(defn- do-get-request
+  "Do a HTTP GET request to a telegram bot API with specified url-suffix and query-params"
+  [{:keys [payload-only] :as params} query-params url-suffix]
+  (let [telegram-api-url (build-telegram-api-url params url-suffix) 
+        response (http/get telegram-api-url
+                           {:accept :json :query-params query-params})]
+    (get-result payload-only response)))
+
 (defn get-updates
-  "Receive updates from telegram Bot via long-polling"
-  [& {:keys [token] :as params}]
-  (let [token (or token (get-token))
-        bot-url (str base-url token "/getUpdates")
-        query-params (merge {:timeout 1
+  "Receive updates from telegram Bot via long-polling. 
+  Supported parameters: :timeout, :offset, :limit.
+
+  For more info look at https://core.telegram.org/bots/api#getupdates
+
+  You might want to get not all http response but telegram API part only - to extract payload part only add ':payload-only true' to arguments"
+  [& {:as params}]
+  (let [query-params (merge {:timeout 1
                              :offset  0
                              :limit   100}
-                            params)
-        resp (http/get bot-url
-                       {:accept :json :query-params query-params})]
-    (-> resp
-        :body
-        (json/read-str :key-fn keyword)
-        :result)))
+                            params)]
+    (do-get-request params query-params "/getUpdates")))
+
+(defn set-webhook
+  "Send setWebhook request to telegram API. For more detailed information look at https://core.telegram.org/bots/api#setwebhook.
+  How to generate self-signed certificate - https://core.telegram.org/bots/self-signed
+
+  You might want to get not all http response but telegram API part only - to extract payload part only add ':payload-only true' to arguments"
+  [& {:keys [url certificate payload-only] :or {url ""} :as params}]
+  (let [telegram-api-url (build-telegram-api-url params "/setWebhook")
+        cert-file (clojure.java.io/file certificate)
+        partial-data [{:name "url" :content url}]
+        multipart-data (if (.exists cert-file)
+                         (conj partial-data {:name  "certificate" :content cert-file})
+                         partial-data)
+        response (http/post telegram-api-url {:accept :json
+                                              :multipart multipart-data})]
+    (get-result payload-only response)))
+
+
+
+
+
+
+
+
+
 
