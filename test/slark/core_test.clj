ba28938317fc9d62e294e429bf6bc6ba8a4db4eb
hsestupin <stupin.sergey@gmail.com>
2016-06-07 14:39:44 +0300

    add send-message tests & change default payload-only -> entire-response?

--- test/slark/core_test.clj
+++ test/slark/core_test.clj
@@ -12,7 +12,7 @@
 
 (deftest get-me-test
   (testing "/getMe raw function"
-    (let [response (get-me)
+    (let [response (get-me :entire-response? true)
           telegram-response (extract-telegram-payload response)]
       (is (not (nil?  response)))
       (is (= 200 (:status response)))
@@ -20,26 +20,54 @@
       (is-clojure-test-bot (:result telegram-response))))
 
   (testing "/getMe payload-only function"
-    (let [telegram-response (get-me :payload-only true)]
+    (let [telegram-response (get-me)]
       (is (:ok telegram-response))
       (is-clojure-test-bot (:result telegram-response))))
 
   (testing "get-me throws returns Unauthorized"
-    (let [telegram-response (get-me :token "1234:abcd" :payload-only true)]
+    (let [telegram-response (get-me :token "1234:abcd")]
       (is (not (:ok telegram-response)))
       (is (= 401 (:error-code telegram-response)))
       (is (= "Unauthorized" (:description telegram-response))))))
 
+(defn- get-chat-id
+  []
+  (Integer/parseInt (env :chat-id)))
+
 (deftest send-message-test
   (testing "simple /sendMessage"
-    ()))
+    (let [message (send-message :chat-id (get-chat-id)
+                                :text "Hi, it's test message.")]
+      (is (:ok message))
+      (is-clojure-test-bot (get-in message [:result :from]))
+      (is (= (get-chat-id) (get-in message [:result :chat :id])))
+      (is (= "Hi, it's test message." (get-in message [:result :text])))))
 
+  (testing "simple /sendMessage silently"
+    (let [message (send-message :chat-id (get-chat-id)
+                                :text "Hi, it's silent test message."
+                                :disable-notification true)]
+      (is (:ok message))
+      (is-clojure-test-bot (get-in message [:result :from]))
+      (is (= (get-chat-id) (get-in message [:result :chat :id])))
+      (is (= "Hi, it's silent test message." (get-in message [:result :text])))))
 
+  (testing "/sendMessage with Markdown parse-mode"
+    (let [message (send-message :chat-id (get-chat-id)
+                                :text "*bold* _italic_ [link](http://google.com)."
+                                :disable-notification true
+                                :parse-mode "Markdown")]
+      (is (:ok message))
+      (is-clojure-test-bot (get-in message [:result :from]))
+      (is (= (get-chat-id) (get-in message [:result :chat :id])))
+      (is (= "bold italic link." (get-in message [:result :text])))))
 
-
-
-
-
-
-
-
+  (testing "/sendMessage with HTML parse-mode"
+    (let [message (send-message :chat-id (get-chat-id)
+                                :text "<b>bold</b> <i>italic</i> <a href=\"http://google.com\">link</a>."
+                                :disable-notification true
+                                :parse-mode "HTML")]
+      (is (:ok message))
+      (is-clojure-test-bot (get-in message [:result :from]))
+      (is (= (get-chat-id) (get-in message [:result :chat :id])))
+      (is (= "bold italic link." (get-in message [:result :text]))))))
